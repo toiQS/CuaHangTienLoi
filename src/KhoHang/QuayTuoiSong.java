4cbac83b51c9144ffd2fb34d3f394cca15261d28
toiQS <88361704+toiQS@users.noreply.github.com>
2021-12-16 22:00:41 +0700

    khắc phục phần static stt trong mỗi quầy

--- src/KhoHang/QuayTuoiSong.java
+++ src/KhoHang/QuayTuoiSong.java
@@ -4,6 +4,7 @@ public class QuayTuoiSong extends KhoHang {
     public static int a = 1; //đếm số lượng mặt hàng có trong quầy
     public QuayTuoiSong(String Ten, String MaSo, String NgayNhapHang, String XuatXu, int SoLuong, float GiaCa){
         super(Ten, MaSo, NgayNhapHang, XuatXu, SoLuong, GiaCa);
+        a+=1;
     }
     @Override
     public String toString(){
