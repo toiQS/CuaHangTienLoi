4d9be4d30365ffcca42d2f0e9c59b88e4f89f685
toiQS <88361704+toiQS@users.noreply.github.com>
2021-12-16 21:59:50 +0700

    Update QuayGiaDung.java

--- src/KhoHang/QuayGiaDung.java
+++ src/KhoHang/QuayGiaDung.java
@@ -4,6 +4,7 @@ public class QuayGiaDung extends KhoHang {
     public static int c = 1; //đếm số lượng mặt hàng có trong quầy
     public QuayGiaDung(String Ten, String MaSo, String NgayNhapHang, String XuatXu, int SoLuong, float GiaCa){
         super(Ten, MaSo, NgayNhapHang, XuatXu, SoLuong, GiaCa);
+        c+=1;
     }
     @Override
     public String toString(){
