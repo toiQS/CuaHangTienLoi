a6c0c65639c60ee9b7ac7e34abd86944e196a2ff
Võ Đăng Quang <mavisstarkvo@gmail.com>
2021-12-16 20:41:10 +0700

    Update BaoVe.java

--- src/NhanSu/BaoVe.java
+++ src/NhanSu/BaoVe.java
@@ -1,12 +1,20 @@
 package NhanSu;
 
-public class BaoVe extends NhanSu{
+public class BaoVe extends NhanSu {
     public static int b = 1;
-    public BaoVe(String Ten, String MaSo, String GioiTinh, int CCCD, String DiaChi, String SDT, String NgayVaoLam){
-        super(Ten, MaSo, GioiTinh, CCCD, DiaChi, SDT, NgayVaoLam);
+
+    public BaoVe(String Ten, String MaSo, String GioiTinh, int CCCD, String DiaChi, String SDT, int SoNgayLam, int Luong) {
+        super(Ten, MaSo, GioiTinh, CCCD, DiaChi, SDT, SoNgayLam, Luong);
     }
+
     @Override
-    public String toString(){
-        return b +".Họ Tên: " + Ten + "/" + "Mã số nhân viên: "+ MaSo +"/Giới Tính: "+ GioiTinh+"/CCCD: "+CCCD+"/Địa Chỉ: "+ DiaChi+"/Ngày Vào Làm: "+NgayVaoLam;
+    public String toString() {
+        return b + ".Họ Tên: " + Ten + "/"
+                + "Mã số nhân viên: " + MaSo
+                + "/Giới Tính: " + GioiTinh
+                + "/CCCD: " + CCCD
+                + "/Địa Chỉ: " + DiaChi
+                + "/Ngày Vào Làm: " + SoNgayLam
+                + "Luong = " + SoNgayLam*176000; //Set Lương mặc định mỗi ngày là 22000
     }
 }
