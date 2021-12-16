93d01a6a70fecd3d068c62f58c62aec18c1a0ab3
Võ Đăng Quang <mavisstarkvo@gmail.com>
2021-12-16 20:41:08 +0700

    Update LaoCong.java

--- src/NhanSu/LaoCong.java
+++ src/NhanSu/LaoCong.java
@@ -2,11 +2,19 @@ package NhanSu;
 
 public class LaoCong extends NhanSu {
     public static int a = 1;
-    public LaoCong(String Ten, String MaSo, String GioiTinh, int CCCD, String DiaChi, String SDT, String NgayVaoLam){
-        super(Ten, MaSo, GioiTinh, CCCD, DiaChi, SDT, NgayVaoLam);
+
+    public LaoCong(String Ten, String MaSo, String GioiTinh, int CCCD, String DiaChi, String SDT, int SoNgayLam, int Luong) {
+        super(Ten, MaSo, GioiTinh, CCCD, DiaChi, SDT, SoNgayLam, Luong);
     }
+
     @Override
-    public String toString(){
-        return a +".Họ Tên: " + Ten + "/" + "Mã số nhân viên: "+MaSo+"/Giới Tính: "+ GioiTinh+"/CCCD: "+CCCD+"/Địa Chỉ: "+ DiaChi+"/Ngày Vào Làm: "+NgayVaoLam;
+    public String toString() {
+        return a + ".Họ Tên: " + Ten + "/"
+                + "Mã số nhân viên: " + MaSo
+                + "/Giới Tính: " + GioiTinh
+                + "/CCCD: " + CCCD
+                + "/Địa Chỉ: " + DiaChi
+                + "/Ngày Vào Làm: " + SoNgayLam
+                + "Luong = " + SoNgayLam * 176000; //Set Lương mặc định mỗi ngày là 22000
     }
 }
