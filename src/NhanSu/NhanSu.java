57f8bcbd210cf7c525deeba6575e2f83d8586e15
Võ Đăng Quang <mavisstarkvo@gmail.com>
2021-12-16 20:41:03 +0700

    Update NhanSu.java

--- src/NhanSu/NhanSu.java
+++ src/NhanSu/NhanSu.java
@@ -2,57 +2,63 @@ package NhanSu;
 
 import Main.ThongTin;
 
-public class NhanSu extends ThongTin{
-   
+public class NhanSu extends ThongTin {
+
     public String GioiTinh;
     public int CCCD;
     public String DiaChi;
     public String SDT;
-    public String NgayVaoLam;
-    
-    public NhanSu(String Ten, String MaSo, String GioiTinh, int CCCD, String DiaChi, String SDT, String NgayVaoLam){
-        super(Ten,MaSo);
+    public int SoNgayLam;
+    public int Luong;
+
+    public NhanSu(String Ten, String MaSo, String GioiTinh, int CCCD, String DiaChi, String SDT, int SoNgayLam, int Luong) {
+        super(Ten, MaSo);
         this.GioiTinh = GioiTinh;
         this.CCCD = CCCD;
         this.DiaChi = DiaChi;
         this.SDT = SDT;
-        this.NgayVaoLam = NgayVaoLam;
-        
+        this.SoNgayLam = SoNgayLam;
+        this.Luong = Luong;
     }
-    
-  
-    public String getGioiTinh(){
+
+    public String getGioiTinh() {
         return GioiTinh;
     }
-    public int getCCCD(){
-        return CCCD;
-    }
-    public String getDiaChi(){
-        return DiaChi;
-    }
-    public String getSDT(){
-        return SDT;
-    }
-    public String getNgayVaoLam(){
-        return NgayVaoLam;
-    }
-    
 
-    
-    public void setGioiTinh(String GioiTinh){
+    public void setGioiTinh(String GioiTinh) {
         this.GioiTinh = GioiTinh;
     }
-    public void setCCCD(int CCCD){
+
+    public int getCCCD() {
+        return CCCD;
+    }
+
+    public void setCCCD(int CCCD) {
         this.CCCD = CCCD;
     }
-    public void setDiaChi(String DiaChi){
+
+    public String getDiaChi() {
+        return DiaChi;
+    }
+
+    public void setDiaChi(String DiaChi) {
         this.DiaChi = DiaChi;
     }
-    public void setSDT(String SDT){
+
+    public String getSDT() {
+        return SDT;
+    }
+
+    public void setSDT(String SDT) {
         this.SDT = SDT;
     }
-    public void getNgayVaoLam(String NgayVaoLam){
-        this.NgayVaoLam = NgayVaoLam;
+
+    public int getSoNgayLam() {
+        return SoNgayLam;
     }
-     
+
+    public int getLuong() {
+        return Luong;
+    }
+
 }
