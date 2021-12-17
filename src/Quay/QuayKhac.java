eb52847ca0c336e777048f46a495c255b5d559a6
toiQS <88361704+toiQS@users.noreply.github.com>
2021-12-17 19:48:56 +0700

    del ham thua

--- src/Quay/QuayKhac.java
+++ src/Quay/QuayKhac.java
@@ -1,5 +1,6 @@
 package Quay;
 
+import java.util.Scanner;
 import KhoHang.KhoHang;
 
 public class QuayKhac extends KhoHang{
@@ -7,6 +8,34 @@ public class QuayKhac extends KhoHang{
     public QuayKhac(String Ten, String MaSo, String NgayNhapHang, String XuatXu, int SoLuong, float GiaCa){
         super(Ten, MaSo, NgayNhapHang, XuatXu, SoLuong, GiaCa);
         d+=1;
+    }
+    public void add(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ten San Pham: ");
+        String Ten = sc.nextLine();
+        System.out.println("Ma san pham: ");
+        String MaSo = sc.nextLine();
+        System.out.println("Ngay Nhap Hang: ");
+        String NgayNhapHang = sc.nextLine();
+        System.out.println("Xuat Xu: ");
+        String XuatXu = sc.nextLine();
+        System.out.println("So Luong Nhap Vao: ");
+        int SoLuong = sc.nextInt();
+        System.out.println("Gia Du Dinh: ");
+        float GiaCa = sc.nextFloat();
+        QuayKhac quayKhac = new QuayKhac(Ten, MaSo, NgayNhapHang, XuatXu, SoLuong, GiaCa);
+        quaykhacList.add(giadung);
+    }
+    public void del(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap Ma So Muon Xoa: ");
+        String MaSo = sc.nextLine();
+        QuayKhac quaykhac = this.quaykhacList.stream().filter(o ->o.getMaSo().equals(MaSo)).findFirst().orElse(null);
+        if(giadung == null){
+            System.out.println("Khong Ton Tai.");
+        }
+        this.giadungList.remove(giadung);
+        System.out.println("Xac Nhan Xoa.");
     }
     @Override
     public String toString(){
--- src/Quay/QuayThucPham.java
+++ src/Quay/QuayThucPham.java
@@ -1,5 +1,5 @@
 package Quay;
-
+import java.util.Scanner;
 import KhoHang.KhoHang;
 
 public class QuayThucPham extends KhoHang{
@@ -7,6 +7,34 @@ public class QuayThucPham extends KhoHang{
     public QuayThucPham(String Ten, String MaSo, String NgayNhapHang, String XuatXu, int SoLuong, float GiaCa){
         super(Ten, MaSo, NgayNhapHang, XuatXu, SoLuong, GiaCa);
         b+=1;
+    }
+    public void add(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ten San Pham: ");
+        String Ten = sc.nextLine();
+        System.out.println("Ma san pham: ");
+        String MaSo = sc.nextLine();
+        System.out.println("Ngay Nhap Hang: ");
+        String NgayNhapHang = sc.nextLine();
+        System.out.println("Xuat Xu: ");
+        String XuatXu = sc.nextLine();
+        System.out.println("So Luong Nhap Vao: ");
+        int SoLuong = sc.nextInt();
+        System.out.println("Gia Du Dinh: ");
+        float GiaCa = sc.nextFloat();
+        QuayThucPham thucpham = new QuayThucPham(Ten, MaSo, NgayNhapHang, XuatXu, SoLuong, GiaCa);
+        thucphamList.add(giadung);
+    }
+    public void del(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap Ma So Muon Xoa: ");
+        String MaSo = sc.nextLine();
+        QuayThucPham thucpham= this.thucphamList.stream().filter(o ->o.getMaSo().equals(MaSo)).findFirst().orElse(null);
+        if(giadung == null){
+            System.out.println("Khong Ton Tai.");
+        }
+        this.thucphamList.remove(giadung);
+        System.out.println("Xac Nhan Xoa.");
     }
     @Override
     public String toString(){
