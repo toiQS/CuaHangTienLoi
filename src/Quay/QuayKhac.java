004632e095bee128ea4946e81bf3bef0b46ff55f
toiQS <88361704+toiQS@users.noreply.github.com>
2021-12-17 19:35:59 +0700

    Create QuayKhac.java

---
+++ src/Quay/QuayKhac.java
@@ -0,0 +1,15 @@
+package Quay;
+
+import KhoHang.KhoHang;
+
+public class QuayKhac extends KhoHang{
+    public static int d = 1; //đếm số lượng mặt hàng có trong quầy
+    public QuayKhac(String Ten, String MaSo, String NgayNhapHang, String XuatXu, int SoLuong, float GiaCa){
+        super(Ten, MaSo, NgayNhapHang, XuatXu, SoLuong, GiaCa);
+        d+=1;
+    }
+    @Override
+    public String toString(){
+        return d +".Ten San Pham: " +Ten+"/Ma San Pham: "+MaSo+"/Xuat Su: "+  XuatXu+"/SoLuong: "+ SoLuong+"/GiaCa: "+ GiaCa;
+    }
+}
