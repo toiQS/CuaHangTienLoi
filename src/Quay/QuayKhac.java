67ffc42f4f4e5b7910093c01780ac866df10bf75
Võ Đăng Quang <mavisstarkvo@gmail.com>
2021-12-18 15:45:28 +0700

    Update QuayKhac.java
    chỉnh giống 2 quầy kia

--- src/Quay/QuayKhac.java
+++ src/Quay/QuayKhac.java
@@ -1,21 +1,23 @@
 package Quay;
 
-import java.io.*;
-import java.util.Scanner;
 import KhoHang.KhoHang;
 
-public class QuayKhac extends KhoHang{
-    public static int d = 1; //đếm số lượng mặt hàng có trong quầy
-    public QuayKhac(String Ten, String MaSo, String NgayNhapHang, String XuatXu, int SoLuong, float GiaCa){
+import java.io.*;
+import java.util.Scanner;
+
+public class QuayKhac extends KhoHang {
+    public static int b = 1; //đếm số lượng mặt hàng có trong quầy
+
+    public QuayKhac(String Ten, String MaSo, String NgayNhapHang, String XuatXu, int SoLuong, float GiaCa) {
         super(Ten, MaSo, NgayNhapHang, XuatXu, SoLuong, GiaCa);
-        d+=1;
+        b += 1;
     }
 
     public QuayKhac() {
 
     }
 
-    public void add(){
+    public void add() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ten San Pham: ");
         String Ten = sc.nextLine();
@@ -33,15 +35,15 @@ public class QuayKhac extends KhoHang{
         quaykhacList.add(quaykhac);
     }
 
-    public void del(){
+    public void del() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Nhap Ma So Muon Xoa: ");
         String MaSo = sc.nextLine();
-        QuayKhac quaykhac = this.quaykhacList.stream().filter(o ->o.getMaSo().equals(MaSo)).findFirst().orElse(null);
-        if(quaykhac == null){
+        QuayKhac quaykhac = quaykhacList.stream().filter(o -> o.getMaSo().equals(MaSo)).findFirst().orElse(null);
+        if (quaykhac == null) {
             System.out.println("Khong Ton Tai.");
         }
-        this.quaykhacList.remove(quaykhac);
+        quaykhacList.remove(quaykhac);
         System.out.println("Xac Nhan Xoa.");
     }
 
@@ -64,7 +66,11 @@ public class QuayKhac extends KhoHang{
     }
 
     @Override
-    public String toString(){
-        return d +".Ten San Pham: " +Ten+"/Ma San Pham: "+MaSo+"/Xuat Su: "+  XuatXu+"/SoLuong: "+ SoLuong+"/GiaCa: "+ GiaCa;
+    public String toString() {
+        return b + ".Ten San Pham: " + Ten + " - "
+                + "Ma San Pham: " + MaSo + " - "
+                + "Xuat Su: " + XuatXu + " - "
+                + "SoLuong: " + SoLuong + " - "
+                + "GiaCa: " + GiaCa;
     }
 }
