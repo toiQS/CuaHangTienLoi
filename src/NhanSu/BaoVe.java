107103a20904e73e874fc967c36383cb8d240df5
Võ Đăng Quang <mavisstarkvo@gmail.com>
2021-12-18 14:07:43 +0700

    Update BaoVe.java

--- src/NhanSu/BaoVe.java
+++ src/NhanSu/BaoVe.java
@@ -1,11 +1,31 @@
 package NhanSu;
 
+import java.util.Scanner;
+
 public class BaoVe extends NhanSu {
     public static int a = 1;
 
-    public BaoVe(String Ten, String MaSo, String GioiTinh, int CCCD, String DiaChi, String SDT, int SoNgayLam, int Luong) {
-        super(Ten, MaSo, GioiTinh, CCCD, DiaChi, SDT, SoNgayLam, Luong);
+    public BaoVe(String Ten, String MaSo, String GioiTinh, String DiaChi, String SDT, int SoNgayLam) {
+        super(Ten, MaSo, GioiTinh, DiaChi, SDT, SoNgayLam);
         a += 1;
+    }
+
+    public void add(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ten nhân viên: ");
+        String Ten = sc.nextLine();
+        System.out.println("Mã nhân viên: ");
+        String MaSo = sc.nextLine();
+        System.out.println("Giới tính: ");
+        String GioiTinh = sc.nextLine();
+        System.out.println("Địa chỉ: ");
+        String DiaChi = sc.nextLine();
+        System.out.println("Số điện thoại: ");
+        String SDT = sc.nextLine();
+        System.out.println("Số ngày làm: ");
+        int SoNgayLam = sc.nextInt();
+        BaoVe baove = new BaoVe(Ten, MaSo, GioiTinh, DiaChi, SDT, SoNgayLam);
+        baoVeList.add(baove);
     }
 
     @Override
@@ -13,7 +33,7 @@ public class BaoVe extends NhanSu {
         return a + ".Họ Tên: " + Ten + "/"
                 + "Mã số nhân viên: " + MaSo
                 + "/Giới Tính: " + GioiTinh
-                + "/CCCD: " + CCCD
+//                + "/CCCD: " + CCCD
                 + "/Địa Chỉ: " + DiaChi
                 + "/Số Ngày Làm: " + SoNgayLam
                 + "Luong = " + SoNgayLam * 176000; //Set Lương mặc định là 22000/giờ, Mỗi ngày làm 8 giờ
