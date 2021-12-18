c851ba1bc30ccee7a5d728448eb112552457862c
Võ Đăng Quang <mavisstarkvo@gmail.com>
2021-12-18 14:27:14 +0700

    Update KhoHang.java

--- src/KhoHang/KhoHang.java
+++ src/KhoHang/KhoHang.java
@@ -1,26 +1,27 @@
 package KhoHang;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-
 import Main.ThongTin;
 import Quay.QuayGiaDung;
 import Quay.QuayKhac;
 import Quay.QuayNuoc;
 import Quay.QuayTuoiSong;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class KhoHang extends ThongTin {
-    public static List<QuayKhac>quaykhacList = new ArrayList<>();
-    public static List<QuayGiaDung>giadungList = new ArrayList<>();
-    public static List<QuayNuoc>nuocList = new ArrayList<>();
-    public static List<QuayTuoiSong>tuoisongList = new ArrayList<>();
+    public static List<QuayKhac> quaykhacList = new ArrayList<>();
+    public static List<QuayGiaDung> giadungList = new ArrayList<>();
+    public static List<QuayNuoc> nuocList = new ArrayList<>();
+    public static List<QuayTuoiSong> tuoisongList = new ArrayList<>();
 
     public String NgayNhapHang;
     public String XuatXu;
     public int SoLuong;
     public float GiaCa; //giá muốn bán
-    public KhoHang(String Ten, String MaSo, String NgayNhapHang, String XuatXu, int SoLuong, float GiaCa){
+
+    public KhoHang(String Ten, String MaSo, String NgayNhapHang, String XuatXu, int SoLuong, float GiaCa) {
         super(Ten, MaSo);
         this.NgayNhapHang = NgayNhapHang;
         this.XuatXu = XuatXu;
@@ -32,40 +33,10 @@ public abstract class KhoHang extends ThongTin {
 
     }
 
-    public String getNgayNhapHang(){
-        return NgayNhapHang;
-    }
-    public void setNgayNhapHang(String NgayNhapHang){
-        this.NgayNhapHang = NgayNhapHang;
-    }
-    public String getXuatXu(){
-        return XuatXu;
-    }
-    public void setXuatXu(String XuatXu){
-        this.XuatXu = XuatXu;
-    }
-    public int getSoLuong(){
-        return SoLuong;
-    }
-    public void setSoLuong(int SoLuong){
-        this.SoLuong = SoLuong;
-    }
-    public float getGiaCa(){
-        return GiaCa;
-    }
-    public void setGiaCa(float GiaCa){
-        this.GiaCa = GiaCa;
-    }
-
-
-    public abstract void add();
-    public abstract void del() ;
-    public abstract void inputFile(String URL) throws IOException;
-
     // Quay gia dung
-    public static void xuatGiaDung(){
+    public static void xuatGiaDung() {
         System.out.println("-----------Quay Gia Dung-------------------");
-        for (QuayGiaDung giaDung: giadungList) {
+        for (QuayGiaDung giaDung : giadungList) {
             System.out.println(giaDung);
         }
     }
@@ -73,7 +44,7 @@ public abstract class KhoHang extends ThongTin {
     // Quay thuc pham
     public static void xuatNuoc() {
         System.out.println("-----------Quay Nuoc-------------------");
-        for (QuayNuoc thucPham: nuocList) {
+        for (QuayNuoc thucPham : nuocList) {
             System.out.println(thucPham);
         }
     }
@@ -81,7 +52,7 @@ public abstract class KhoHang extends ThongTin {
     //Quay tuoi song
     public static void xuatTuoiSong() {
         System.out.println("-----------Quay Tuoi Song-------------------");
-        for (QuayTuoiSong tuoiSong: tuoisongList) {
+        for (QuayTuoiSong tuoiSong : tuoisongList) {
             System.out.println(tuoiSong);
         }
     }
@@ -89,7 +60,7 @@ public abstract class KhoHang extends ThongTin {
     //Quay khac
     public static void xuatKhac() {
         System.out.println("-----------Quay Khac-------------------");
-        for (QuayKhac khac: quaykhacList) {
+        for (QuayKhac khac : quaykhacList) {
             System.out.println(khac);
         }
     }
@@ -102,29 +73,67 @@ public abstract class KhoHang extends ThongTin {
 
         //in quay tuoi song
         outputStreamWriter.write("------------Quay Tuoi Song---------------\n");
-        for (QuayTuoiSong tuoiSong: tuoisongList) {
+        for (QuayTuoiSong tuoiSong : tuoisongList) {
             outputStreamWriter.write(String.valueOf(tuoiSong));
             outputStreamWriter.write("\n");
         }
         outputStreamWriter.flush();
         outputStreamWriter.write("\n\n------------Quay Gia Dung---------------\n");
-        for (QuayGiaDung giaDung: giadungList) {
+        for (QuayGiaDung giaDung : giadungList) {
             outputStreamWriter.write(String.valueOf(giaDung));
             outputStreamWriter.write("\n");
         }
         outputStreamWriter.flush();
         outputStreamWriter.write("\n\n------------Quay Tuoi Song---------------\n");
-        for (QuayNuoc nuoc: nuocList) {
+        for (QuayNuoc nuoc : nuocList) {
             outputStreamWriter.write(String.valueOf(nuoc));
             outputStreamWriter.write("\n");
         }
 
         outputStreamWriter.flush();
         outputStreamWriter.write("\n\n------------Quay Khac---------------\n");
-        for (QuayKhac khac: quaykhacList) {
+        for (QuayKhac khac : quaykhacList) {
             outputStreamWriter.write(String.valueOf(khac));
             outputStreamWriter.write("\n");
         }
         outputStreamWriter.flush();
     }
+
+    public String getNgayNhapHang() {
+        return NgayNhapHang;
+    }
+
+    public void setNgayNhapHang(String NgayNhapHang) {
+        this.NgayNhapHang = NgayNhapHang;
+    }
+
+    public String getXuatXu() {
+        return XuatXu;
+    }
+
+    public void setXuatXu(String XuatXu) {
+        this.XuatXu = XuatXu;
+    }
+
+    public int getSoLuong() {
+        return SoLuong;
+    }
+
+    public void setSoLuong(int SoLuong) {
+        this.SoLuong = SoLuong;
+    }
+
+    public float getGiaCa() {
+        return GiaCa;
+    }
+
+    public void setGiaCa(float GiaCa) {
+        this.GiaCa = GiaCa;
+    }
+
+    public abstract void add();
+
+    public abstract void del();
+
+    public abstract void inputFile(String URL) throws IOException;
 }
