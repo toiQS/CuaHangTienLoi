ef76e631d16d98ed2c277a8c02d6fe16d45d4632
Võ Đăng Quang <mavisstarkvo@gmail.com>
2021-12-18 14:07:50 +0700

    Update NhanVien.java

--- src/NhanSu/NhanVien.java
+++ src/NhanSu/NhanVien.java
@@ -1,11 +1,31 @@
 package NhanSu;
 
+import java.util.Scanner;
+
 public class NhanVien extends NhanSu {
     public static int stt = 1;
 
-    public NhanVien(String Ten, String MaSo, String GioiTinh, int CCCD, String DiaChi, String SDT, int SoNgayLam, int Luong) {
-        super(Ten, MaSo, GioiTinh, CCCD, DiaChi, SDT, SoNgayLam, Luong);
+    public NhanVien(String Ten, String MaSo, String GioiTinh, String DiaChi, String SDT, int SoNgayLam) {
+        super(Ten, MaSo, GioiTinh, DiaChi, SDT, SoNgayLam);
         stt += 1;
+    }
+
+    public void add(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ten nhân viên: ");
+        String Ten = sc.nextLine();
+        System.out.println("Mã nhân viên: ");
+        String MaSo = sc.nextLine();
+        System.out.println("Giới tính: ");
+        String GioiTinh = sc.nextLine();
+        System.out.println("Địa chỉ: ");
+        String DiaChi = sc.nextLine();
+        System.out.println("Số điện thoại: ");
+        String SDT = sc.nextLine();
+        System.out.println("Số ngày làm: ");
+        int SoNgayLam = sc.nextInt();
+        NhanVien nhanvien = new NhanVien(Ten, MaSo, GioiTinh, DiaChi, SDT, SoNgayLam);
+        nhanVienList.add(nhanvien);
     }
 
     @Override
@@ -13,7 +33,7 @@ public class NhanVien extends NhanSu {
         return stt + ".Họ Tên: " + Ten + "/"
                 + "Mã số nhân viên: " + MaSo
                 + "/Giới Tính: " + GioiTinh
-                + "/CCCD: " + CCCD
+//                + "/CCCD: " + CCCD
                 + "/Địa Chỉ: " + DiaChi
                 + "/Số Ngày Làm: " + SoNgayLam
                 + "Luong = " + SoNgayLam * 176000; //Set Lương mặc định là 22000/giờ, Mỗi ngày làm 8 giờ
