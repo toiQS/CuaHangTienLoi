bda9902aa5216c646c0d5ab8ff654ab36bfc5539
Võ Đăng Quang <mavisstarkvo@gmail.com>
2021-12-18 15:59:29 +0700

    Update NhanVien.java

--- src/NhanSu/NhanVien.java
+++ src/NhanSu/NhanVien.java
@@ -5,37 +5,40 @@ import java.util.Scanner;
 public class NhanVien extends NhanSu {
     public static int stt = 1;
 
-    public NhanVien(String Ten, String MaSo, String GioiTinh, String DiaChi, String SDT, int SoNgayLam) {
-        super(Ten, MaSo, GioiTinh, DiaChi, SDT, SoNgayLam);
+    public NhanVien(String Ten, String MaSo, String CCCD, String GioiTinh, String DiaChi, String SDT, float Luong) {
+        super(Ten, MaSo, CCCD, GioiTinh, DiaChi, SDT, Luong);
         stt += 1;
     }
 
+    @Override
     public void add() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ten nhân viên: ");
+        System.out.println("Tên nhân viên: ");
         String Ten = sc.nextLine();
         System.out.println("Mã nhân viên: ");
         String MaSo = sc.nextLine();
+        System.out.println("Căn cước công dân: ");
+        String CCCD = sc.nextLine();
         System.out.println("Giới tính: ");
         String GioiTinh = sc.nextLine();
         System.out.println("Địa chỉ: ");
         String DiaChi = sc.nextLine();
         System.out.println("Số điện thoại: ");
         String SDT = sc.nextLine();
-        System.out.println("Số ngày làm: ");
-        int SoNgayLam = sc.nextInt();
-        NhanVien nhanvien = new NhanVien(Ten, MaSo, GioiTinh, DiaChi, SDT, SoNgayLam);
+        System.out.println("Lương: ");
+        float Luong = sc.nextInt();
+        NhanVien nhanvien = new NhanVien(Ten, MaSo, CCCD, GioiTinh, DiaChi, SDT, Luong);
         nhanvienList.add(nhanvien);
     }
 
     @Override
     public String toString() {
-        return stt + ".Họ Tên: " + Ten + "/"
-                + "Mã số nhân viên: " + MaSo
-                + "/Giới Tính: " + GioiTinh
-//                + "/CCCD: " + CCCD
-                + "/Địa Chỉ: " + DiaChi
-                + "/Số Ngày Làm: " + SoNgayLam
-                + "Luong = " + SoNgayLam * 176000; //Set Lương mặc định là 22000/giờ, Mỗi ngày làm 8 giờ
+        return stt + ".Họ Tên: " + Ten + " - "
+                + "Mã số nhân viên: " + MaSo + " - "
+                + "CCCD: " + CCCD + " - "
+                + "Giới Tính: " + GioiTinh + " - "
+                + "Địa Chỉ: " + DiaChi + " - "
+                + "Số điện thoại: " + SDT + " - "
+                + "Luong = " + Luong * 8 * 30; //Set mỗi ngày làm 8 giờ và lương tính 1 tháng 30 ngày.
     }
 }
