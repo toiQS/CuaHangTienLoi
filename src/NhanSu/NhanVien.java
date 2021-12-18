48d2b14e9ec62a8ba9816bb03d7113cfdcfc4cac
Võ Đăng Quang <mavisstarkvo@gmail.com>
2021-12-18 10:43:47 +0700

    Update NhanVien.java

--- src/NhanSu/NhanVien.java
+++ src/NhanSu/NhanVien.java
@@ -1,20 +1,21 @@
 package NhanSu;
 
 public class NhanVien extends NhanSu {
-    public static int b = 1;
+    public static int stt = 1;
 
     public NhanVien(String Ten, String MaSo, String GioiTinh, int CCCD, String DiaChi, String SDT, int SoNgayLam, int Luong) {
         super(Ten, MaSo, GioiTinh, CCCD, DiaChi, SDT, SoNgayLam, Luong);
+        stt += 1;
     }
 
     @Override
     public String toString() {
-        return b + ".Họ Tên: " + Ten + "/"
+        return stt + ".Họ Tên: " + Ten + "/"
                 + "Mã số nhân viên: " + MaSo
                 + "/Giới Tính: " + GioiTinh
                 + "/CCCD: " + CCCD
                 + "/Địa Chỉ: " + DiaChi
                 + "/Số Ngày Làm: " + SoNgayLam
-                + "Luong = " + SoNgayLam*176000; //Set Lương mặc định là 22000/giờ, Mỗi ngày làm 8 giờ
+                + "Luong = " + SoNgayLam * 176000; //Set Lương mặc định là 22000/giờ, Mỗi ngày làm 8 giờ
     }
 }
