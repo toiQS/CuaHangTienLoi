24112e7f878740b2e3e6e871fbd95e89d0c7055a
Võ Đăng Quang <mavisstarkvo@gmail.com>
2021-12-18 10:25:54 +0700

    Create LaoCong.java

---
+++ src/NhanSu/LaoCong.java
@@ -0,0 +1,20 @@
+package NhanSu;
+
+public class LaoCong extends NhanSu {
+    public static int b = 1;
+
+    public LaoCong(String Ten, String MaSo, String GioiTinh, int CCCD, String DiaChi, String SDT, int SoNgayLam, int Luong) {
+        super(Ten, MaSo, GioiTinh, CCCD, DiaChi, SDT, SoNgayLam, Luong);
+    }
+
+    @Override
+    public String toString() {
+        return b + ".Họ Tên: " + Ten + "/"
+                + "Mã số nhân viên: " + MaSo
+                + "/Giới Tính: " + GioiTinh
+                + "/CCCD: " + CCCD
+                + "/Địa Chỉ: " + DiaChi
+                + "/Số Ngày Làm: " + SoNgayLam
+                + "Luong = " + SoNgayLam*176000; //Set Lương mặc định là 22000/giờ, Mỗi ngày làm 8 giờ
+    }
+}
