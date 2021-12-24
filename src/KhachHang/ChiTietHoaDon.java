6e478677abedc163602bf678b98e5bb7d25d0dd4
toiQS <88361704+toiQS@users.noreply.github.com>
2021-12-24 11:12:21 +0700

    thêm thời gian

--- src/KhachHang/ChiTietHoaDon.java
+++ src/KhachHang/ChiTietHoaDon.java
@@ -1,11 +1,14 @@
 package KhachHang;
-
+import java.util.Date;
 
 public class ChiTietHoaDon extends XuLy {
 
     // Xuất list hóa đơn + giá tổng
     public void xuatHoaDon(){
+
         System.out.println("       ---------------------------------------------------------");
+        Date date = new Date();
+
         System.out.println();
         for(XuLy HOADON : XuLy.ListHoaDon)
             if(HOADON != null)
@@ -13,5 +16,6 @@ public class ChiTietHoaDon extends XuLy {
         System.out.println();
         System.out.println("       ---------------------------------------------------------");
         System.out.println("                       Tổng tiền  : "+ XuLy.tongGia );
+        System.out.println("                                         "+date.toString());
     }
 }
